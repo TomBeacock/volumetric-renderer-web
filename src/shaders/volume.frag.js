52a2a1da1bd6879ce7cfe42c184ccada57454429
Thomas Beacock <thomasbeacock@gmail.com>
2024-02-19 16:23:37 +0000

    Flipped normals for volume camera clipping

--- src/shaders/volume.frag.js
+++ src/shaders/volume.frag.js
@@ -18,31 +18,25 @@ uniform sampler2D u_transfer_func;
 void main() {
     vec4 color = vec4(0.0, 0.0, 0.0, 1.0);
     vec3 ray_dir = normalize(frag_position - u_camera_position);
-    vec3 ray_pos = frag_tex_coords;
+    vec3 ray_pos = frag_tex_coords - ray_dir * 1.8;
 
     float ray_dist = 1.8;
     float step_size = 0.005;
     int steps = int(ray_dist / step_size);
 
     for (int i = 0; i < steps; i++) {
-        if (any(greaterThan(ray_pos, vec3(1.0, 1.0, 1.0))) ||
-            any(lessThan(ray_pos, vec3(0.0, 0.0, 0.0)))) {
-            break;
-        }
-
-        if (all(lessThan(ray_pos, u_slice_max)) &&
-            all(greaterThan(ray_pos, u_slice_min))) {
+        if (all(greaterThan(ray_pos, u_slice_min)) ||
+            all(lessThan(ray_pos, u_slice_max))) {
             float density = texture(u_volume, ray_pos).r;
             float t = (density - u_density_min) / (u_density_max - u_density_min);
             vec4 sample_color = texture(u_transfer_func, vec2(t, 0.5));
             color.rgb += color.a * (sample_color.a * sample_color.rgb);
             color.a *= (1.0 - sample_color.a);
-        }
 
-        if (color.a < 0.001) {
-            break;
+            if (color.a < 0.001) {
+                break;
+            }
         }
-
         ray_pos += ray_dir * step_size;
     }
     color.a = 1.0 - color.a;
